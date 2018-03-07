3e92cf98a5cb152755608b71fb4dc44b2862db69
Julia Spivakova <julia.snp@gmail.com>
2018-03-07 12:12:55 +0000

    username and pw storage

--- HealthProfileApp.java
+++ HealthProfileApp.java
@@ -8,9 +8,8 @@ import java.util.Scanner;
  */
 public class HealthProfileApp {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+     
     public static void main(String[] args) {
 
     String firstName;
